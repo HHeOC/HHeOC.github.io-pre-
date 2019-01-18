23661fa9a008bcae25c777e34447a178ef5c8b17
ZhengChen He <31530891+HHeOC@users.noreply.github.com>
2019-01-18 12:15:34 -1000

    Add files via upload

--- store.js
+++ store.js
@@ -1,10 +1,10 @@
-document.getElementById('food1Name').innerHTML = "Curry CHICKEN KATSU";
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function(){
   if(this.readyState == 4 && this.status == 200){
     var Storedata = JSON.parse(this.responseText);
     document.getElementById('food1Name').innerHTML = Storedata.food[0].name;
+     document.getElementById('food1price').innerHTML = Storedata.food[0].price;
   }
 };
-xmlhttp.open("GET" , "store.json",true);
+xmlhttp.open("GET" , "store.json", true);
 xmlhttp.send();
