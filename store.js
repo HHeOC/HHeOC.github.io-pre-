8a95a1c6037d99ff26a7ba667dd41f234cc5709c
ZhengChen He <31530891+HHeOC@users.noreply.github.com>
2019-01-18 12:01:37 -1000

    Update store.js

--- store.js
+++ store.js
@@ -1,10 +1,1 @@
 document.getElementById('food1Name').innerHTML = "Curry CHICKEN KATSU";
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function(){
-  if(this.readyState == 4 && this.status == 200){
-    var Storedata = JSON.parse(this.responseText);
-    document.getElementById('food1Name').innerHTML = Storedata.food[0].name;
-  }
-};
-xmlhttp.open("GET" , "store.json",true);
-xmlhttp.send();
