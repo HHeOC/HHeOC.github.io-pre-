bc55b2369068fcb021d8de918a5d2ba2b666626b
ZhengChen He <31530891+HHeOC@users.noreply.github.com>
2019-01-25 11:16:18 -1000

    Update store.js

--- store.js
+++ store.js
@@ -3,7 +3,7 @@ xmlhttp.onreadystatechange = function(){
   if(this.readyState == 4 && this.status == 200){
     var Storedata = JSON.parse(this.responseText);
     document.getElementById('food1Name').innerHTML = Storedata.foods[0].name;
-     document.getElementById('food1price').innerHTML = Storedata.foods[0].price;
+     document.getElementById('food1price').innerHTML = "$" + Storedata.foods[0].price;
   }
 };
 xmlhttp.open("GET" , "store.json", true);
