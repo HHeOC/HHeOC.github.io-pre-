8bbe2c0d619781e056d5a3e030f584eb5dbae395
ZhengChen He <31530891+HHeOC@users.noreply.github.com>
2019-02-02 18:43:22 -1000

    Add files via upload

--- store.js
+++ store.js
@@ -2,60 +2,49 @@
 var foods =[];
 var prices= [];
 var tax = 1.04712;
-localStorage.setItem('Store1', "");
-localStorage.setItem('Store2', "");
-localStorage.setItem('Store3', "");
-localStorage.setItem('Store4', "");
-localStorage.setItem('Store5', "");
-localStorage.setItem('Store6', "");
-localStorage.setItem('P1', "");
-localStorage.setItem('P2', "");
-localStorage.setItem('P3', "");
-localStorage.setItem('P4', "");
-localStorage.setItem('P5', "");
-localStorage.setItem('P6', "");
+
 //JSON file function. attacch json objects
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function(){
   if(this.readyState == 4 && this.status == 200){
     var Storedata = JSON.parse(this.responseText);
     //food number 1
-    var foodN1= Storedata.foods[0].name;
+    var foodN1 = Storedata.foods[0].name;
     foods.push(foodN1);
     var priceD1 = Storedata.foods[0].price;
     prices.push(priceD1);
     document.getElementById('food1Name').innerHTML = Storedata.foods[0].name;
     document.getElementById('food1price').innerHTML = "$" + Storedata.foods[0].price;
     //food number 2
-    var foodN2= Storedata.foods[1].name;
+    var foodN2 = Storedata.foods[1].name;
     foods.push(foodN2);
     var priceD2 = Storedata.foods[1].price;
     prices.push(priceD2);
     document.getElementById('food2Name').innerHTML = Storedata.foods[1].name;
     document.getElementById('food2price').innerHTML = "$" + Storedata.foods[1].price;
     //food number 3
-    var foodN3= Storedata.foods[2].name;
+    var foodN3 = Storedata.foods[2].name;
     foods.push(foodN3);
     var priceD3 = Storedata.foods[2].price;
     prices.push(priceD3);
     document.getElementById('food3Name').innerHTML = Storedata.foods[2].name;
     document.getElementById('food3price').innerHTML = "$" + Storedata.foods[2].price;
     //food number 4
-    var foodN4= Storedata.foods[3].name;
+    var foodN4 = Storedata.foods[3].name;
     foods.push(foodN4);
     var priceD4 = Storedata.foods[3].price;
     prices.push(priceD4);
     document.getElementById('food4Name').innerHTML = Storedata.foods[3].name;
     document.getElementById('food4price').innerHTML = "$" + Storedata.foods[3].price;
     //food number 5
-    var foodN5= Storedata.foods[4].name;
+    var foodN5 = Storedata.foods[4].name;
     foods.push(foodN5);
     var priceD5 = Storedata.foods[4].price;
     prices.push(priceD5);
     document.getElementById('food5Name').innerHTML = Storedata.foods[4].name;
     document.getElementById('food5price').innerHTML = "$" + Storedata.foods[4].price;
     //food number 6
-    var foodN6= Storedata.foods[5].name;
+    var foodN6 = Storedata.foods[5].name;
     foods.push(foodN6);
     var priceD6 = Storedata.foods[5].price;
     prices.push(priceD6);
@@ -78,20 +67,20 @@ function ShowResult1(){
   var F1P = T1P.toFixed(2);
      var Save1 = Result1 + " " + foods[0] + " $" + F1P;
     localStorage.setItem('Store1', Save1);
-    var load1 = localStorage.getItem('Store1');
+    var load1 = localStorage.getItem('Store1'); 
     localStorage.setItem('P1', F1P);
-  if(Result1 >0){
+  if((Result1 > 0) && (Result1 < 5)){
   window.alert('Item has been added, Please scroll down to finish your purchase');
     document.getElementById('show1').innerHTML = load1;
-  }
-  else if(Result1 == ""){
-  window.alert('please select your quantity');
+}
+else if(Result1 == 0){
+    window.alert("quantity can't be zero");
+ 
   }
   else{
-    window.alert("quantity can't be zero");
+    window.alert('please select quantity that is allowed');
   }
-  localStorage.setItem("foodS1", F1P);
-  AP1=0;
+
 }
 
 //button 2
@@ -106,15 +95,16 @@ function ShowResult2(){
   localStorage.setItem('Store2', Save2);
   var load2 = localStorage.getItem('Store2');
   localStorage.setItem('P2', F2P);
-  if(Result2 >0){
-  window.alert('Item has been added, Please scroll down to finish your purchase');
-    document.getElementById('show2').innerHTML = load2;
+  if ((Result2 > 0) && (Result2 < 5)) {
+    window.alert('Item has been added, Please scroll down to finish your purchase');
+    document.getElementById('show1').innerHTML = load1;
   }
-    else if(Result2 == ""){
-  window.alert('please select your quantity');
-  }
-  else{
+  else if (Result2 == 0) {
     window.alert("quantity can't be zero");
+
+  }
+  else {
+    window.alert('please select quantity that is allowed');
   }
 }
 
@@ -128,18 +118,18 @@ function ShowResult3(){
   var F3P = T3P.toFixed(2);
      var Save3 = Result3 + " " + foods[2] + " $" + F3P;
     localStorage.setItem('Store3', Save3);
-    var load3 = localStorage.getItem('Store3');
+    var load3 = localStorage.getItem('Store3'); 
     localStorage.setItem('P3', F3P);
-  if(Result3 >0){
+  if ((Result3 > 0) && (Result3 < 5)) {
     window.alert('Item has been added, Please scroll down to finish your purchase');
-
-    document.getElementById('show3').innerHTML = load3;
+    document.getElementById('show1').innerHTML = load1;
   }
-    else if(Result3 === ""){
-  window.alert('please select your quantity');
-  }
-  else{
+  else if (Result3 == 0) {
     window.alert("quantity can't be zero");
+
+  }
+  else {
+    window.alert('please select quantity that is allowed');
   }
 }
 
@@ -155,15 +145,16 @@ function ShowResult4(){
   localStorage.setItem('Store4', Save4);
   var load4 = localStorage.getItem('Store4');
   localStorage.setItem('P4', F4P);
-  if(Result4 >0){
+  if ((Result4 > 0) && (Result4 < 5)) {
     window.alert('Item has been added, Please scroll down to finish your purchase');
- document.getElementById('show4').innerHTML = load4;
+    document.getElementById('show1').innerHTML = load1;
   }
-    else if(Result4 == ""){
-  window.alert('please select your quantity');
-  }
-  else{
+  else if (Result4 == 0) {
     window.alert("quantity can't be zero");
+
+  }
+  else {
+    window.alert('please select quantity that is allowed');
   }
 }
 
@@ -179,18 +170,17 @@ function ShowResult5(){
   localStorage.setItem('Store5', Save5);
   var load5 = localStorage.getItem('Store5');
   localStorage.setItem('P5', F5P);
-  if(Result5 >0){
-  window.alert('Item has been added, Please scroll down to finish your purchase');
-  document.getElementById('show5').innerHTML = load5;
+  if ((Result5 > 0) && (Result5 < 5)) {
+    window.alert('Item has been added, Please scroll down to finish your purchase');
+    document.getElementById('show1').innerHTML = load1;
   }
-    else if(Result5 == ""){
-  window.alert('please select your quantity');
-  }
-  else{
+  else if (Result5 == 0) {
     window.alert("quantity can't be zero");
+
   }
- 
- 
+  else {
+    window.alert('please select quantity that is allowed');
+  }
 }
 
 //button 6
@@ -205,17 +195,19 @@ function ShowResult6(){
   localStorage.setItem('Store6', Save6);
   var load6 = localStorage.getItem('Store6');
   localStorage.setItem('P6', F6P);
-  if(Result6 >0){
+  if ((Result6 > 0) && (Result6 < 5)) {
     window.alert('Item has been added, Please scroll down to finish your purchase');
-  document.getElementById('show6').innerHTML = load6;
+    document.getElementById('show1').innerHTML = load1;
   }
-    else if(Result6 == ""){
-  window.alert('please select your quantity');
-  }
-  else{
+  else if (Result6 == 0) {
     window.alert("quantity can't be zero");
+
+  }
+  else {
+    window.alert('please select quantity that is allowed');
   }
 }
+
 
 
 //page2 java script codes
@@ -233,64 +225,93 @@ var AP5 = Number(localStorage.getItem('P5'));
 var AP6 = Number(localStorage.getItem('P6'));
 var ToTal = AP1 + AP2 + AP3 + AP4 + AP5 + AP6;
 var sTotal = ToTal.toFixed(2);
-var olist = document.getElementById('Slist');
-
+var ol = document.getElementById('Slist');
 //add item
 //first
-document.getElementById('Slist').innerHTML = AP1;
 var li1 = document.createElement('li');
 var liText1 = document.createTextNode(load1);
 li1.appendChild(liText1);
-olist.appendChild(li1);
-if(AP1 <= 0 ){
-   olist.removeChild(li1);
+ol.appendChild(li1);
+if (AP1 <= 0) {
+  ol.removeChild(li1);
 }
 //second
 var li2 = document.createElement('li');
 var liText2 = document.createTextNode(load2);
 li2.appendChild(liText2);
-olist.appendChild(li2);
+ol.appendChild(li2);
 if (AP2 <= 0) {
-  olist.removeChild(li2);
+  ol.removeChild(li2);
 }
 //third
 var li3 = document.createElement('li');
 var liText3 = document.createTextNode(load3);
 li3.appendChild(liText3);
-olist.appendChild(li3);
+ol.appendChild(li3);
 if (AP3 <= 0) {
-  olist.removeChild(li3);
+  ol.removeChild(li3);
 }
 //forth
 var li4 = document.createElement('li');
 var liText4 = document.createTextNode(load4);
 li4.appendChild(liText4);
-olist.appendChild(li4);
+ol.appendChild(li4);
 if (AP4 <= 0) {
-  olist.removeChild(li4);
+  ol.removeChild(li4);
 }
 //fifth
 var li5 = document.createElement('li');
 var liText5 = document.createTextNode(load5);
 li5.appendChild(liText5);
-olist.appendChild(li5);
+ol.appendChild(li5);
 if (AP5 <= 0) {
-  olist.removeChild(li5);
+  ol.removeChild(li5);
 }
 //sixth
 var li6 = document.createElement('li');
 var liText6 = document.createTextNode(load6);
 li6.appendChild(liText6);
-olist.appendChild(li6);
+ol.appendChild(li6);
 if (AP6 <= 0) {
-  olist.removeChild(li6);
+  ol.removeChild(li6);
 }
+
 //subtotal
-document.getElementById('subtotal').innerHTML = "Your total is " + "$" +sTotal;
+document.getElementById('subtotal').innerHTML = "Your total is " + "$" + sTotal;
+//reset all
 //button transform to next page
 function nextpage() {
  location.href = "result.html";
 }
-function Return(){
+function Return() {
   location.href = "index.html";
 }
+function mean() {
+  window.alert("Total Cost : $ " + sTotal);
+  window.alert(" Thank you for your purchase! have a good day! ");
+  location.href = "index.html";
+}
+function kind() { 
+  var Ft = sTotal * 1.15;
+  window.alert("Total Cost : $ " + Ft.toFixed(2));
+  window.alert(" Thank you for your purchase! have a good day! ");
+  location.href = "index.html";
+}
+function veryKind() {
+  var Ft = sTotal * 1.30;
+  window.alert("Total Cost : $ " + Ft.toFixed(2));
+  window.alert(" Thank you for your purchase! have a good day! ");
+  location.href = "index.html";
+}
+localStorage.setItem('Store1', "");
+localStorage.setItem('Store2', "");
+localStorage.setItem('Store3', "");
+localStorage.setItem('Store4', "");
+localStorage.setItem('Store5', "");
+localStorage.setItem('Store6', "");
+localStorage.setItem('P1', "");
+localStorage.setItem('P2', "");
+localStorage.setItem('P3', "");
+localStorage.setItem('P4', "");
+localStorage.setItem('P5', "");
+localStorage.setItem('P6', "");
